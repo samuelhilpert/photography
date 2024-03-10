9bc5c9da4a4958e668df0a0b7df55d985fb27cf4
Samuel <hilpertsamuel@web.de>
2024-03-10 10:53:06 +0100

    add file structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,9 @@
       <div class="bg-gray-200 p-4 rounded-md shadow-sm w-64 mx-auto">
         <h1 class="text-center text-xl font-bold">Hallo {name}!</h1>
       </div>
-
+    <div>
+        <a href="/auto" class="text-blue-600 hover:text-blue-800 underline"
+        >Click here to go checkout our colorscheme</a
+        >
+    </div>
 </main>
