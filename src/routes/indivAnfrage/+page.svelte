d2ac31b075d778d6ba60835e72b2d9a24a8b899f
Samuel <hilpertsamuel@web.de>
2024-03-15 14:24:13 +0100

    email individuelle Version

--- src/routes/indivAnfrage/+page.svelte
+++ src/routes/indivAnfrage/+page.svelte
@@ -1,36 +1,82 @@
 <script>
+
+    import auto from "$lib/img/1709463717041.jpg";
+    import pferd from "$lib/img/img.png";
+    import mensch from "$lib/img/img_1.png";
+    import pferdbewegung from "$lib/img/IMG_1462.png";
+    import hundportrait from "$lib/img/img_2.png";
+    import pferdmensch from "$lib/img/IMG_5141-Edit.jpg";
+
     export let form;
-    let test ="Moin";
-    import {Input, Textarea, AccordionItem, Accordion} from "flowbite-svelte";
-    let textareaContent = "";
+    let test ="";
+    import {Input, Textarea, AccordionItem, Accordion, Label, Checkbox, Radio, Button} from "flowbite-svelte";
+
     let getvor = "";
     let getnach = "";
-    let textareaContent1 = "";
     let getmail1 = "";
     let getdate1 = "";
-    let getanmerkungen = "";
+    let getbild = "";
+    let getstunden = "";
 
-    function inhaltarea2(event) {
-        textareaContent1 = event.target.value;
-    }
-    function inhaltarea1(event) {
-        textareaContent = event.target.value;
-    }
+
     function getvornamen(event) {
         getvor = event.target.value;
+        updateTest();
     }
     function getnachnamen(event) {
         getnach = event.target.value;
+        updateTest();
     }
     function getmail(event) {
         getmail1 = event.target.value;
+        updateTest();
     }
     function getdate(event) {
         getdate1 = event.target.value;
+        updateTest();
     }
-    function anmerkungen(event) {
-        getanmerkungen = event.target.value;
+    function getBilder(event) {
+        getbild = event.target.value;
+        updateTest();
     }
+    function getStunden(event) {
+        getstunden = event.target.value;
+        updateTest();
+    }
+    function updateTest() {
+        test = `Du hast eine neue Anfrage von ${getvor} ${getnach} erhalten. Hier sind die Details: Der Kunde wünscht sich ein Shooting mit einem ${selectedCategory}. Die gewünschte Bildart ist ${selectedtier}. Der Kunde wünscht sich ${getbild} Bilder und will ${getstunden} Stunden shooten. Der Kunde kann am ${getdate1} das Shooting durchführen und seine Mail-Adresse lautet ${getmail1}.`;
+    }
+
+    let selectedCategory;
+    let selectedtier;
+    let isAutoSelected = false;
+    let isTierSelected = false;
+    let isMenschSelected = false;
+
+    function handleRadioChange(event) {
+        const selectedValue = event.target.value;
+
+        switch (selectedValue) {
+            case 'Auto':
+                isAutoSelected = true;
+                isTierSelected = false;
+                isMenschSelected = false;
+                break;
+            case 'Tier':
+                isAutoSelected = false;
+                isTierSelected = true;
+                isMenschSelected = false;
+                break;
+            case 'Mensch':
+                isAutoSelected = false;
+                isTierSelected = false;
+                isMenschSelected = true;
+
+                break;
+
+        }
+    }
+
 </script>
 <main class="content" style="position: relative">
         <div class="container h-full mx-auto flex justify-center items-center mt-4">
@@ -43,22 +89,126 @@
         <div class="container flex flex-row justify-content: center gap-4 pl-32 pr-32 pt-4" style="width: 4000px">
             <div class="flex-1 border rounded-lg shadow-md bg-text ">
                 <Accordion>
-                    <AccordionItem>
-                        <span slot="header">My Header 1</span>
-                        <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-                        <p class="text-gray-500 dark:text-gray-400">
-                            Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
-                            and start developing websites even faster with components on top of Tailwind CSS.
-                        </p>
+                    <AccordionItem open>
+                        <span slot="header">Welches Motiv stellst du dir vor?</span>
+                        <div class="container mx-auto flex flex-row justify-content: center gap-4">
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={auto} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Radio color="red" name="category" class="flex justify-center text-lg" bind:group={selectedCategory} on:change={handleRadioChange} value="Auto">Auto</Radio>
+
+                            </div>
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={pferd} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Radio  color="red" name="category" class="flex justify-center text-lg" bind:group={selectedCategory} on:change={handleRadioChange} value="Tier">Tier</Radio>
+
+                            </div>
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={mensch} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Radio color="red" name="category" class="flex justify-center text-lg" bind:group={selectedCategory}  value="Mensch">Mensch</Radio>
+                            </div>
+                        </div>
                     </AccordionItem>
                     <AccordionItem>
-                        <span slot="header">My Header 2</span>
+                        <span slot="header">Was stellst du dir da vor? (Mehrfachauswahl möglich)</span>
+
+                        {#if isTierSelected}
+                        <div  class="container mx-auto flex flex-row justify-content: center gap-4">
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={pferdbewegung} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Checkbox color="red" class="flex justify-center text-lg" bind:group={selectedtier}  value="in Bewegung">in Bewegung</Checkbox>
+
+
+                            </div>
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={hundportrait} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Checkbox color="red" class="flex justify-center text-lg" bind:group={selectedtier} value="Portrait">Portrait</Checkbox>
+
+
+                            </div>
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={pferdmensch} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Checkbox color="red" class="flex justify-center text-lg" bind:group={selectedtier} value="Portrait mit Mensch">Portrait mit Mensch</Checkbox>
+
+                            </div>
+                        </div>
+                        {/if}
+                        {#if isAutoSelected}
+
+                        <div class="container mx-auto flex flex-row justify-content: center gap-4">
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={auto} alt='banner' class="h-40 rounded-lg mx-auto"/>
+                                <Checkbox color="red" class="flex justify-center text-lg" bind:group={selectedtier}  value="inBewegung">in Bewegung</Checkbox>
+
+                            </div>
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={auto} alt='banner' class="h-40 rounded-lg mx-auto"/>
+
+
+                            </div>
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={auto} alt='banner' class="h-40 rounded-lg mx-auto"/>
+
+                            </div>
+                        </div>
+                        {/if}
+                        {#if isMenschSelected}
+                        <div class="container mx-auto flex flex-row justify-content: center gap-4">
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={mensch} alt='banner' class="h-40 rounded-lg mx-auto"/>
+
+                            </div>
+
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={mensch} alt='banner' class="h-40 rounded-lg mx-auto"/>
+
+
+                            </div>
+                            <div class="flex-1 p-4 border rounded-lg shadow-md bg-text text-center">
+                                <img src={mensch} alt='banner' class="h-40 rounded-lg mx-auto"/>
+
+                            </div>
+                        </div>
+                        {/if}
+
+                    </AccordionItem>
+                    <AccordionItem>
+                        <span slot="header">Welchen Umfang stellst du dir vor?</span>
+                        <div class="container mx-auto flex flex-row justify-content: center gap-4">
+
+                            <div class="flex flex-row p-4 border rounded-lg shadow-md bg-text text-center">
+                                <p>Anzahl Stunden</p>
+                                <Input on:input={getStunden} type="number" class="w-full"/>
+                            </div>
+
+
+                            <div class="flex flex-row p-4 border rounded-lg shadow-md bg-text text-center">
+                                <p>Anzahl Bilder</p>
+                                <Input on:input={getBilder} type="number" class="w-full"/>
+                            </div>
+                        </div>
+
+                    </AccordionItem>
+                    <AccordionItem>
+                        <span slot="header">Kontaktdaten</span>
                         <div class="flex w-full  rounded-lg   pl-3 pr-3">
                             <div class="flex gap-4 w-full mt-5 mb-5">
                                 <Input on:input={getvornamen} id="vorname" size="lg" placeholder="Vorname" class="w-full" />
                                 <Input on:input={getnachnamen} id="nachname" size="lg" placeholder="Nachname" class="w-full" />
-                                <Input on:input={getdate} type="date" class="w-full" />
+                                <Label class="my-auto">Shooting-Tag</Label>
+                                <Input on:input={getdate} type="date" class="w-full"/>
                             </div>
+                        </div>
+                        <div class="container mx-auto flex flex-row justify-content: center gap-4">
+
+
+                            <Input on:input={getmail} type=mail id="mail" size="lg" placeholder="E-Mail" class="w-full m-3" />
                         </div>
                     </AccordionItem>
                 </Accordion>
@@ -71,6 +221,14 @@
 
 
         </div>
+    <p class="text-accent">{selectedCategory}</p>
+    <p class="text-accent">{selectedtier}</p>
+    <p class="text-accent">{getvor}</p>
+    <p class="text-accent">{getnach}</p>
+    <p class="text-accent">{getmail1}</p>
+    <p class="text-accent">{getdate1}</p>
+    <p class="text-accent">{getstunden}</p>
+    <p class="text-accent">{getbild}</p>
     <p class="success text-accent">{form?.success || ""}</p>
     <div class="container h-full mx-auto flex justify-center items-center mt-4">
         <h2>
@@ -89,7 +247,7 @@
                     </div>
                     <div class="input invisible">
                         <label for="">Subject:</label>
-                        <input name="subject" type="text" value="Du hast eine neue Anfrage" />
+                        <input name="subject" type="text" value="Du hast eine neue Anfrage: Individuelle Anfrage" />
                     </div>
                     <div class="input invisible">
                         <label for="">Body:</label>
