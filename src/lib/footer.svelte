1f286a31097ce7e7b9093fffbecf6944a4791de4
svenjavol <svenjavolmering1@gmail.com>
2024-04-06 12:11:28 +0200

    footer anpassung der links

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -6,16 +6,17 @@
   </script>
   
   <Footer footerType="logo" class="bg-secondary w-full overflow-hidden">
-    <div class="h-1/7 sm:flex sm:items-center sm:justify-between">
+    <div class=" sm:flex sm:items-center sm:justify-between">
       <FooterBrand href="/+page" src={logo} alt="logo"/>
-      <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-test sm:mb-0 dark:text-text">
+      <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-text sm:mb-0 dark:text-text h-1/6">
         <FooterLink href="/">Portfolio</FooterLink>
-        <FooterLink href="/">Datenschutz/Impressum</FooterLink>
-#        <FooterLink href="/">Info</FooterLink>
+        <FooterLink href="/impressum">Impressum</FooterLink>
+        <FooterLink href="/datenschutz">Datenschutz</FooterLink>
+        <FooterLink href="/impre">Informationen</FooterLink>
       </FooterLinkGroup>
 
    
     </div>
-    <hr class="my-6 border-primary sm:mx-auto dark:border-primary lg:my-8"/>
+    <hr class="my-6 pb-0 border-primary sm:mx-auto dark:border-primary lg:my-8 "/>
     <FooterCopyright by="SaSv Photography™" class="text-text " href="/"/>
 </Footer>
