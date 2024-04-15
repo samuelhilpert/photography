eaf72f26005d75b8d396b7e3c645344410aad4ed
svenjavol <svenjavolmering1@gmail.com>
2024-04-15 11:09:29 +0200

    Code Aufräume

--- src/lib/menu.svelte
+++ src/lib/menu.svelte
@@ -1,40 +1,41 @@
 <script>
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu, Button } from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+	import logo from '$lib/img/Logo.jpg';
 
-    import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu, Button } from 'flowbite-svelte';
-    import { ChevronDownOutline } from 'flowbite-svelte-icons';
-    import logo from '$lib/img/Logo.jpg';
+	let menu = [
+		{ name: 'Autos', href: '/autoPortfolio' },
+		{ name: 'Pferde', href: '/pferd' },
+		{ name: 'Menschen', href: '/mensch' },
+		{ name: 'Hunde', href: '/hund' }
+	];
+</script>
 
-    let menu = [
-        {name: 'Autos', href: '/autoPortfolio'},
-        {name: 'Pferde', href: '/pferd'},
-        {name: 'Menschen', href: '/mensch'},
-        {name: 'Hunde', href: '/hund'}
-    ];
-
-  </script>
-  <div class="w-full">
-  <Navbar class="bg-background border-secondary border-4 rounded-lg" let:hidden let:toggle>
-    <NavBrand href="/">
-      <img alt="logo" src={logo} class="rounded-full h-1/6 w-1/12">
-      <span class=" whitespace-nowrap text-text text-xl font-semibold ">SaSv Photography</span>
-
-    </NavBrand>
-    <NavHamburger on:click={toggle}/>
-    <NavUl {hidden} >
-        <NavLi class="text-text  hover:text-accent " href="/">Home</NavLi>
-        <NavLi class="cursor-pointer text-text hover:text-accent ">
-            Portfolio
-            <ChevronDownOutline class=" hover:text-accent  w-3 h-3 ms-2  text-text dark:text-text inline"/>
-        </NavLi>
-        <MegaMenu class="w-1/5 mt-6 rounded-lg bg-background text-text z-50 " items={menu} let:item>
-            <a class=" hover:text-accent  hover:underline hover:text-accent-600 dark:hover:text-primary-500" href={item.href}>
-                {item.name}
-            </a>
-        </MegaMenu>
-        <NavLi class="text-text hover:text-accent " href="/overviewAnfrage">Anfragen</NavLi>
-        <NavLi class="text-text hover:text-accent " href="/impre">Info</NavLi>
-    </NavUl>
-
-  </Navbar>
+<div class="w-full">
+	<Navbar class="rounded-lg border-4 border-secondary bg-background" let:hidden let:toggle>
+		<NavBrand href="/">
+			<img alt="logo" src={logo} class="h-1/6 w-1/12 rounded-full" />
+			<span class=" whitespace-nowrap text-xl font-semibold text-text">SaSv Photography</span>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
+			<NavLi class="text-text  hover:text-accent " href="/">Home</NavLi>
+			<NavLi class="cursor-pointer text-text hover:text-accent ">
+				Portfolio
+				<ChevronDownOutline
+					class=" ms-2  inline h-3 w-3  text-text hover:text-accent dark:text-text"
+				/>
+			</NavLi>
+			<MegaMenu class="z-50 mt-6 w-1/5 rounded-lg bg-background text-text " items={menu} let:item>
+				<a
+					class=" hover:text-accent-600 dark:hover:text-primary-500 hover:text-accent hover:underline"
+					href={item.href}
+				>
+					{item.name}
+				</a>
+			</MegaMenu>
+			<NavLi class="text-text hover:text-accent " href="/overviewAnfrage">Anfragen</NavLi>
+			<NavLi class="text-text hover:text-accent " href="/impre">Info</NavLi>
+		</NavUl>
+	</Navbar>
 </div>
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,133 +1,126 @@
 <script>
-    let name = 'Svenja';
-    import {Gallery} from 'flowbite-svelte';
+	let name = 'Svenja';
+	import { Gallery } from 'flowbite-svelte';
 
-    /**Import der Bilder
-     * --TODO:
-     */
-    import bannerIdee from '$lib/img/BannerIdee.jpg';
-    import imgauto from '$lib/img/auto.jpg';
-    import hund from '$lib/img/hund.jpg';
-    import mensch from '$lib/img/mensch.jpg';
-    import pferd from '$lib/img/pferd.jpg';
+	//Import der Bilder
 
-    const imgPferd = [{alt: 'Pferd', src: pferd}];
-    const imgAuto = [{alt: 'Auto', src: imgauto}];
-    const imgMensch = [{alt: 'Mensch', src: mensch}];
-    const imgHund = [{alt: 'Hund', src: hund}];
-    const images1 = [{alt: 'Banner', src: bannerIdee}];
+	import bannerIdee from '$lib/img/BannerIdee.jpg';
+	import imgauto from '$lib/img/auto.jpg';
+	import hund from '$lib/img/hund.jpg';
+	import mensch from '$lib/img/mensch.jpg';
+	import pferd from '$lib/img/pferd.jpg';
 
-    /**functions für dynamisches erscheinen der Titel
-     * --TODO: hover über schrift s/w
-     */
+	const imgPferd = [{ alt: 'Pferd', src: pferd }];
+	const imgAuto = [{ alt: 'Auto', src: imgauto }];
+	const imgMensch = [{ alt: 'Mensch', src: mensch }];
+	const imgHund = [{ alt: 'Hund', src: hund }];
+	const images1 = [{ alt: 'Banner', src: bannerIdee }];
 
-    function handleAutoClick() {
-        window.location.assign('/autoPortfolio');
-    }
+	//functions für dynamisches erscheinen der Titel
 
-    function handlePferdClick() {
-        window.location.assign('/pferd');
-    }
+	function handleAutoClick() {
+		window.location.assign('/autoPortfolio');
+	}
 
-    function handleMenschClick() {
-        window.location.assign('/mensch');
-    }
+	function handlePferdClick() {
+		window.location.assign('/pferd');
+	}
 
-    function handleHundClick() {
-        window.location.assign('/hund');
-    }
+	function handleMenschClick() {
+		window.location.assign('/mensch');
+	}
+
+	function handleHundClick() {
+		window.location.assign('/hund');
+	}
 </script>
 
 <main class="content" style="position: relative ">
-    <div class="bg-background">
-        <div class="banner">
-            <img alt="banner" class="rounded-lg brightness-75" src={bannerIdee}/>
-            <div class="text-overlay">Photography</div>
-            <div class="text-overlay-untertitel">create memories</div>
-        </div>
-        <div class="ml-2.5 mr-2.5 mt-3.5">
-            <Gallery class="scale-95 grid-cols-2 gap-4 md:grid-cols-4 ">
-                <button class="kategorieBezeichnung" on:click={handleAutoClick}>
-                    <Gallery items={imgAuto}/>
-                    <div class="textAuto"> Autos</div>
-                    <div class="textAutoU"> Mehr erfahren!</div>
-                </button>
-                <button class="kategorieBezeichnung" on:click={handlePferdClick}>
-                    <Gallery items={imgPferd}/>
-                    <div class="textAuto"> Pferde</div>
-                    <div class="textAutoU"> Mehr erfahren!</div>
-                </button>
-                <button class="kategorieBezeichnung" on:click={handleMenschClick}>
-                    <Gallery items={imgMensch}/>
-                    <div class="textAuto"> Menschen</div>
-                    <div class="textAutoU"> Mehr erfahren!</div>
-                </button>
-                <button class="kategorieBezeichnung" on:click={handleHundClick}>
-                    <Gallery items={imgHund}/>
-                    <div class="textAuto"> Hunde</div>
-                    <div class="textAutoU"> Mehr erfahren!</div>
-                </button>              
-            </Gallery>
-        </div>
-    </div>
-
+	<div class="bg-background">
+		<div class="banner">
+			<img alt="banner" class="rounded-lg brightness-75" src={bannerIdee} />
+			<div class="text-overlay">Photography</div>
+			<div class="text-overlay-untertitel">create memories</div>
+		</div>
+		<div class="ml-2.5 mr-2.5 mt-3.5">
+			<Gallery class="scale-95 grid-cols-2 gap-4 md:grid-cols-4 ">
+				<button class="kategorieBezeichnung" on:click={handleAutoClick}>
+					<Gallery items={imgAuto} />
+					<div class="textAuto">Autos</div>
+					<div class="textAutoU">Mehr erfahren!</div>
+				</button>
+				<button class="kategorieBezeichnung" on:click={handlePferdClick}>
+					<Gallery items={imgPferd} />
+					<div class="textAuto">Pferde</div>
+					<div class="textAutoU">Mehr erfahren!</div>
+				</button>
+				<button class="kategorieBezeichnung" on:click={handleMenschClick}>
+					<Gallery items={imgMensch} />
+					<div class="textAuto">Menschen</div>
+					<div class="textAutoU">Mehr erfahren!</div>
+				</button>
+				<button class="kategorieBezeichnung" on:click={handleHundClick}>
+					<Gallery items={imgHund} />
+					<div class="textAuto">Hunde</div>
+					<div class="textAutoU">Mehr erfahren!</div>
+				</button>
+			</Gallery>
+		</div>
+	</div>
 </main>
 
 <style>
-    /**Platzierung des Banners*/
-    /** Platzierung des Banners*/
-    .banner {
-        @apply justify-center rounded-lg;
-        margin-top: 2%;
-        margin-left: 5%;
-        margin-right: 5%;
-    }
+	/**Platzierung des Banners*/
+	.banner {
+		@apply justify-center rounded-lg;
+		margin-top: 2%;
+		margin-left: 5%;
+		margin-right: 5%;
+	}
 
-    /**Platzierung der Schrift*/
+	/**Platzierung der Schrift*/
 
-    .text-overlay {
-        position: absolute;
-        @apply text-text;
-        top: 15%; /* Vertikal zentrieren */
-        left: 32%; /* Horizontal zentrieren */
-        transform: translate(-50%, -50%); /* Zentrierung anpassen */
-        font-size: 100px;
-        /* Weitere Stile nach Bedarf */
-    }
+	.text-overlay {
+		position: absolute;
+		@apply text-text;
+		top: 15%;
+		left: 32%;
+		transform: translate(-50%, -50%);
+		font-size: 100px;
+	}
 
-    .text-overlay-untertitel {
-        position: absolute;
-        @apply font-serif text-text;
-        top: 25%; /* Vertikal zentrieren */
-        left: 63%; /* Horizontal zentrieren */
-        transform: translate(-50%, -50%); /* Zentrierung anpassen */
-        font-size: 50px;
-    }
+	.text-overlay-untertitel {
+		position: absolute;
+		@apply font-serif text-text;
+		top: 25%;
+		left: 63%;
+		transform: translate(-50%, -50%);
+		font-size: 50px;
+	}
 
-    /**Dynamischer Text über den Kategoriebildern*/
+	/**Dynamischer Text über den Kategoriebildern*/
 
-    .textAuto {
-        @apply relative -translate-y-64 text-2xl font-semibold text-text brightness-200;
-        opacity: 0;
-    }
+	.textAuto {
+		@apply relative -translate-y-64 text-2xl font-semibold text-text brightness-200;
+		opacity: 0;
+	}
 
-    .textAutoU {
-        @apply relative -translate-y-64 text-base text-text brightness-200;
-        opacity: 0;
-    }
+	.textAutoU {
+		@apply relative -translate-y-64 text-base text-text brightness-200;
+		opacity: 0;
+	}
 
-    
-    .kategorieBezeichnung:hover .textAuto {
-        opacity: 1;
-        scale: 1;
-    }
+	.kategorieBezeichnung:hover .textAuto {
+		opacity: 1;
+		scale: 1;
+	}
 
-    .kategorieBezeichnung:hover .textAutoU {
-        opacity: 1;
-        scale: 1;
-    }
-  
-    .kategorieBezeichnung {
-        @apply hover:grayscale hover:brightness-75;
-    }
+	.kategorieBezeichnung:hover .textAutoU {
+		opacity: 1;
+		scale: 1;
+	}
+
+	.kategorieBezeichnung {
+		@apply hover:brightness-75 hover:grayscale;
+	}
 </style>
